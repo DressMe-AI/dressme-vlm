6096ce1edf8e2733c2de68cbb9e0de2dd9a0e83f
Erim Yanik <90459089+yaniker@users.noreply.github.com>
2025-06-05 14:04:29 -0400

    removed artefacts

--- config_semantics.py
+++ config_semantics.py
@@ -1,11 +1,5 @@
 # config_semantics.yaml
 
-data_dir: "./data/images"
 resized_dir: "./data/resized"
 output_path: "./output/semantics.json"
 prompt_path: "./prompts/semantics_prompt.txt"
-
-image_size:
-  - 256
-  - 256
-
